ac9fd5027fe457bdeeb7e706d2b64a3850810501
jillantony <38492594+jillantony@users.noreply.github.com>
2018-08-09 06:45:39 +0530

    Update contourdetection.py

--- contourdetection.py
+++ contourdetection.py
@@ -1,5 +1,15 @@
 import cv2
-import numpy as np
+import numpy as npdef detectContours(image):
+	greyImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	edges = cv2.Canny(greyImage,30,200)
+	
+	
+#funtion half only findcontours and draw contour left
+def detectContours(image):
+	greyImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	edges = cv2.Canny(greyImage,30,200)
+	
+	
 capture = cv2.VideoCapture(0)
 while(True):
 	ret, frame = capture.read()
